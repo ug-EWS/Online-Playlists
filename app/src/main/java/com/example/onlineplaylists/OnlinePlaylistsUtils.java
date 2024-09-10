e9722f529f38cf652b1b5dfd82517dc40f40d0ce
ug-EWS <ugur.guler2003@gmail.com>
2024-09-10 12:24:39 +0300

    commit

--- app/src/main/java/com/example/onlineplaylists/OnlinePlaylistsUtils.java
+++ app/src/main/java/com/example/onlineplaylists/OnlinePlaylistsUtils.java
@@ -109,8 +109,6 @@ public class OnlinePlaylistsUtils {
                 if ("primary".equalsIgnoreCase(type)) {
                     return Environment.getExternalStorageDirectory() + "/" + split[1];
                 }
-
-                // TODO handle non-primary volumes
             }
             // DownloadsProvider
             else if (isDownloadsDocument(uri)) {
